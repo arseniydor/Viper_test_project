f3317707dfdb81e9c6303fecba8ea48c556203f8
Arseniy Dorogin <dorogin.arseniy@yandex.ru>
2022-04-20 11:28:34 +0300

    Initial Commit

---
+++ WaveAccessTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaveAccessTestApp
+//
+//  Created by Arseniy Dorogin on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
